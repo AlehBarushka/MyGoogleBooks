e6dce9801c77f5f059859a9dc960db8f151509e8
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-06-29 22:16:48 +0300

    Add pagination (#6)
    * Added actions, and saga. Updated reducer
    
    * Added pagination in component

--- src/actionCreators/index.js
+++ src/actionCreators/index.js
@@ -1,4 +1,11 @@
-import { GET_BOOKS_LIST_FAILURE, GET_BOOKS_LIST_PENDING, GET_BOOKS_LIST_SUCCESS } from '../actions';
+import {
+  GET_BOOKS_LIST_FAILURE,
+  GET_BOOKS_LIST_PENDING,
+  GET_BOOKS_LIST_SUCCESS,
+  GET_MORE_BOOKS_FAILURE,
+  GET_MORE_BOOKS_PENDING,
+  GET_MORE_BOOKS_SUCCESS,
+} from '../actions';
 
 export const getBooks = ({ searchTerm, category, page, sort }) => ({
   type: GET_BOOKS_LIST_PENDING,
@@ -12,5 +19,20 @@ export const getBooksSuccess = data => ({
 
 export const getBooksFailure = error => ({
   type: GET_BOOKS_LIST_FAILURE,
+  payload: error,
+});
+
+export const getMoreBooks = ({ searchTerm, category, page, sort }) => ({
+  type: GET_MORE_BOOKS_PENDING,
+  payload: { searchTerm, category, page, sort },
+});
+
+export const getMoreBooksSuccess = data => ({
+  type: GET_MORE_BOOKS_SUCCESS,
+  payload: data,
+});
+
+export const getMoreBooksFailure = error => ({
+  type: GET_MORE_BOOKS_FAILURE,
   payload: error,
 });
