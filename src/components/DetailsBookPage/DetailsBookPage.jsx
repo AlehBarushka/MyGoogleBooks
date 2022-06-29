68104c8161be68723866ed3ddd3b8842ea4a6cb6
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-06-29 11:51:46 +0300

    Finish details book page (#5)
    * Deleted unnecessary
    
    * Finish details book page

--- src/components/DetailsBookPage/DetailsBookPage.jsx
+++ src/components/DetailsBookPage/DetailsBookPage.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import { Container, Col, Button, Row, Image } from 'react-bootstrap';
-import bookImg from '../../assets/book-sample.jpg';
+
+import noCover from '../../assets/no_cover_thumb.gif';
 
 const DetailsBookPage = () => {
+  const { books } = useSelector(state => state.volumeList);
   const navigate = useNavigate();
+
+  let { id } = useParams();
+
+  //This effect is intended to redirect to the home page after reloading the details book page
+  useEffect(() => {
+    if (books?.length === 0) {
+      navigate('/', { replace: true });
+    }
+  }, [books, navigate]);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  const currentBook = books.find(e => e.id === id);
+
+  const authors = currentBook?.volumeInfo.authors?.join(', ');
+  const img = currentBook?.volumeInfo.imageLinks?.thumbnail;
+  const title = currentBook?.volumeInfo.title;
+  const categories = currentBook?.volumeInfo.categories;
+  const description = currentBook?.volumeInfo.description;
 
   const handleOnClick = () => {
     navigate('/books/list');
@@ -15,17 +39,13 @@ const DetailsBookPage = () => {
     <Container>
       <Row xs={1} md={2} className='mt-5'>
         <Col className='p-3 d-flex justify-content-center img-container'>
-          <Image className='shadow' src={bookImg} />
+          <Image className='shadow' src={img || noCover} />
         </Col>
         <Col className='pt-2 px-5'>
-          <div className='fw-light mb-3'>Books category</div>
-          <h3 className='mb-2'>Books title</h3>
-          <div className='fw-lighter text-decoration-underline mb-2'>Books Authors</div>
-          <p className='fw-light'>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio natus labore
-            excepturi velit, minima voluptas, placeat modi, ducimus veritatis a provident ut. Neque,
-            accusantium enim? Officiis autem minus suscipit debitis?
-          </p>
+          <div className='fw-light mb-3'>{categories}</div>
+          <h3 className='mb-2'>{title}</h3>
+          <div className='fw-lighter text-decoration-underline mb-2'>{authors}</div>
+          <p className={`fw-light ${description && 'border'} rounded p-2`}>{description}</p>
         </Col>
       </Row>
       <div className='text-center'>
