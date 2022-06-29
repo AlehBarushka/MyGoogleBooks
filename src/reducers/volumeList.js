a65e65abe7d4a9974aabdfe90dc2106fa4be9261
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-06-29 22:15:36 +0300

    Added actions, and saga. Updated reducer

--- src/reducers/volumeList.js
+++ src/reducers/volumeList.js
@@ -1,19 +1,46 @@
-import { GET_BOOKS_LIST_FAILURE, GET_BOOKS_LIST_PENDING, GET_BOOKS_LIST_SUCCESS } from '../actions';
+import {
+  GET_BOOKS_LIST_FAILURE,
+  GET_BOOKS_LIST_PENDING,
+  GET_BOOKS_LIST_SUCCESS,
+  GET_MORE_BOOKS_FAILURE,
+  GET_MORE_BOOKS_PENDING,
+  GET_MORE_BOOKS_SUCCESS,
+} from '../actions';
 
-const initialState = { isLoading: false, books: [], total: 0, error: null };
+const initialState = {
+  isLoading: false,
+  isLoadingButton: false,
+  books: [],
+  total: 0,
+  error: null,
+  searchTerm: {},
+};
 
 const volumeListReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case GET_BOOKS_LIST_PENDING:
       return { ...state, isLoading: true };
 
+    case GET_MORE_BOOKS_PENDING:
+      return { ...state, isLoadingButton: true };
+
+    case GET_MORE_BOOKS_SUCCESS:
+      return {
+        ...state,
+        isLoadingButton: false,
+        searchTerm: payload.searchTerm,
+        books: [...state.books, ...payload.items],
+        error: null,
+      };
+
     case GET_BOOKS_LIST_SUCCESS:
       return {
         ...state,
-        error: null,
         isLoading: false,
-        books: payload.items,
         total: payload.totalItems,
+        searchTerm: payload.searchTerm,
+        books: payload.items,
+        error: null,
       };
 
     case GET_BOOKS_LIST_FAILURE:
@@ -21,6 +48,13 @@ const volumeListReducer = (state = initialState, { type, payload }) => {
         ...state,
         error: payload,
         isLoading: false,
+      };
+
+    case GET_MORE_BOOKS_FAILURE:
+      return {
+        ...state,
+        error: payload,
+        isLoadingButton: false,
       };
 
     default:
--- src/sagas/volumeList.js
+++ src/sagas/volumeList.js
@@ -2,19 +2,41 @@ import { takeEvery, put, call, all } from 'redux-saga/effects';
 
 import { googleBooksApi } from '../services';
 
-import { getBooksSuccess, getBooksFailure } from '../actionCreators';
-import { GET_BOOKS_LIST_PENDING } from '../actions';
+import {
+  getBooksSuccess,
+  getBooksFailure,
+  getMoreBooksSuccess,
+  getMoreBooksFailure,
+} from '../actionCreators';
+import { GET_BOOKS_LIST_PENDING, GET_MORE_BOOKS_PENDING } from '../actions';
 
 function* getBooks({ payload }) {
   try {
-    const { totalItems, items } = yield call(googleBooksApi.getBooks, payload);
+    const searchTerm = payload;
 
-    yield put(getBooksSuccess({ totalItems, items }));
+    const { totalItems, items } = yield call(googleBooksApi.getBooks, searchTerm);
+
+    yield put(getBooksSuccess({ totalItems, items, searchTerm }));
   } catch ({ response: { data } }) {
     yield put(getBooksFailure(data.error));
   }
 }
 
+function* getMoreBooks({ payload }) {
+  try {
+    const searchTerm = payload;
+
+    const { totalItems, items } = yield call(googleBooksApi.getBooks, searchTerm);
+
+    yield put(getMoreBooksSuccess({ totalItems, items, searchTerm }));
+  } catch ({ response: { data } }) {
+    yield put(getMoreBooksFailure(data.error));
+  }
+}
+
 export function volumeListSaga() {
-  return all([takeEvery(GET_BOOKS_LIST_PENDING, getBooks)]);
+  return all([
+    takeEvery(GET_BOOKS_LIST_PENDING, getBooks),
+    takeEvery(GET_MORE_BOOKS_PENDING, getMoreBooks),
+  ]);
 }
